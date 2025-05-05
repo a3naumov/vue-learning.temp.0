ee854ac3f95bd2b53097d9011386f2fbe543cf02
Artem Naumov <naumovartem272@gmail.com>
2025-05-05 16:39:24 +0400

    Computed properties

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,68 +1,52 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import {computed, reactive, ref} from 'vue'
 
-const count = ref(0)
-console.log(count.value)
+const author = reactive({
+  name: 'John Doe',
+  books: ['Vue 2 - Advanced Guider', 'Vue 3 - Basic Guide', 'Vue 4 - The msytery'],
+})
+
+const publishedBooksMessage = computed(() => {
+  return author.books.length > 0 ? 'Yes' : 'No'
+})
+console.log(publishedBooksMessage)
+
+
+
+const firstName = ref('John')
+const lastName = ref('Doe')
+
+const fullName = computed({
+  get() {
+    return firstName.value + ' ' + lastName.value
+  },
+  set(newValue) {
+    [firstName.value, lastName.value] = newValue.split(' ')
+  }
+})
+fullName.value = 'John Doe'
+console.log(firstName.value)
+console.log(lastName.value)
+
+
+const count = ref(2)
+const alwaysSmall = computed((prevValue) => {
+  console.log(prevValue)
+  return count.value;
+})
 count.value++
-console.log(count)
-
-function increment() {
-  count.value++
-}
-
-const state = reactive({ count: 0 })
-console.log(state)
-
-const message: string = 'Hello World!'
-
-const html = '<span style="color: red">This should be red.</span>'
-
-let customId = 1
-customId++
-
-const objectOfAttrs = {
-  id: 'container',
-  class: 'wrapper',
-  style: 'background-color:green',
-}
-
-const seen = true
-
-const event = 'click'
+console.log(alwaysSmall.value)
+count.value++
+console.log(alwaysSmall.value)
 </script>
 
 <template>
   <main>
-    <button @click="increment">
-      {{ count }}
-    </button>
-
-    <button @click="state.count++">
-      {{ state.count }}
-    </button>
-
-    <h1>{{ message }}</h1>
-
     <div>
-      <p>Using text interpolation: {{ html }}</p>
-      <p>Using v-html directive <span v-html="html"></span></p>
+      <p>Has published books:</p>
+      <span>{{ publishedBooksMessage }}</span>
     </div>
-
-    <div v-bind:id="'custom-id-' + customId.toString()"></div>
-    <div :id="'custom-short-hand-id-' + customId.toString()"></div>
-
-    <div v-bind="objectOfAttrs"></div>
-
-    <p v-if="seen">Now you see mee</p>
-
-    <button v-on:click="console.log(1)"></button>
-    <button @click="console.log(1)"></button>
-
-    <button v-on:[event]="console.log(2)"></button>
-    <button @[event]="console.log(2)"></button>
-
-    <button v-on:submit.prevent="console.log(3)"></button>
-    <button @click.once="console.log(3)"></button>
+    <button @click="author.books = []"></button>
   </main>
 </template>
 
