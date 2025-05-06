4249b11497b4fa7ba8b16dce64891ec0ae9f53ed
Artem Naumov <naumovartem272@gmail.com>
2025-05-06 18:01:13 +0400

    List rendering

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,45 +1,63 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 
-const awesome = ref(true)
+const parentMessage = ref('Parent message');
+const items = [
+  {id:0, message: 'Foo'},
+  {id: 1,message: 'Bar'},
+]
 
-const type = 'C'
+const myObject = reactive({
+  title: 'How to do lists in Vue',
+  author: 'Jane Doe',
+  publishedAt: '2016-04-10',
+})
 </script>
 
 <template>
   <main>
     <div>
-      <button @click="awesome = !awesome"></button>
-
-      <h1 v-if="awesome">Vue is awesome</h1>
-      <h1 v-else>Oh no</h1>
+      <ul>
+        <li v-for="item in items">
+          {{item.message}}
+        </li>
+      </ul>
+      <ul>
+        <li v-for="(item, index) in items">
+          {{ parentMessage}} - {{index}} - {{ item.message}}
+        </li>
+      </ul>
+      <ul>
+        <li v-for="({message}, index) in items">
+          {{ message }} {{index}}
+        </li>
+      </ul>
     </div>
 
     <div>
-      <div v-if="type === 'A'">
-        A
-      </div>
-      <div v-else-if="type === 'B'">
-        B
-      </div>
-      <div v-else-if="type === 'C'">
-        C
-      </div>
-      <div v-else>
-        Not A/B/C
-      </div>
+      <ul>
+        <li v-for="(value, key, index) in myObject">
+          {{ index }}. {{ key }}: {{ value }}
+        </li>
+      </ul>
     </div>
 
     <div>
-      <template v-if="true">
-        <h1>Title</h1>
-        <p>Paragraph 1</p>
-        <p>Paragraph 2</p>
+      <!-- 1-10 -->
+      <span v-for="n in 10">{{n}}</span>
+    </div>
+
+    <div>
+      <template v-for="item in items">
+        <li>{{item.message}}</li>
+        <li class="divider" role="presentation"></li>
       </template>
     </div>
 
     <div>
-      <h1 v-show="false">Hello</h1>
+      <template v-for="item in items" :key="item.id">
+        <li>{{item.message}}</li>
+      </template>
     </div>
   </main>
 </template>
