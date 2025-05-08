9a0185668594a6dff0511a372b6a1107ebbd1bbc
Artem Naumov <naumovartem272@gmail.com>
2025-05-08 23:31:23 +0400

    Watchers

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,84 +1,57 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {ref, watch, watchEffect} from "vue";
 
-const message = ref('')
+const question = ref('')
+const answer = ref('Questions usually contain a question mark. ;-)')
+const loading = ref(false)
+watch(question, async (newQuestion, oldQuestion) => {
+  if (newQuestion.includes('?')) {
+    loading.value = true
+    answer.value = 'Thinking...'
+    try {
+      const res = await fetch('https://yesno.wtf/api')
+      answer.value = (await res.json()).answer
+    } catch (error) {
+      answer.value = 'Error! Could not reach the API. ' + error
+    } finally {
+      loading.value = false
+    }
+  }
+})
 
-const checked = ref(false)
 
-const checkedNames = ref([])
 
-const picked = ref('')
-
-const selected = ref('A')
-const options = ref([
-  { text: 'One', value: 'A' },
-  { text: 'Two', value: 'B' },
-  { text: 'Three', value: 'C' }
-])
-
-const toggle = ref(false)
-const dynamicTrueValue = ref('yes1')
-const dynamicFalseValue = ref('no2')
+const x = ref(0)
+const y = ref(0)
+watch(x, (newX) => {
+  console.log(`x is ${newX}`)
+}, {once: true})
+watch(
+  () => x.value + y.value,
+  (sum) => {
+    console.log(`sum of x + y is: ${sum}`)
+  }
+)
+watch(
+  [x, () => y.value],
+  ([newX, newY]) => {
+    console.log(`x is ${newX} and y is ${newY}`)
+  }
+)
 </script>
 
 <template>
   <main>
     <div>
-      <p>Message is: {{ message }}</p>
-      <input v-model="message" placeholder="edit me" />
+      <p>
+        Ask a yes/no question:
+        <input v-model="question" :disabled="loading">
+      </p>
+      <p>{{answer}}</p>
     </div>
-
     <div>
-      <span>Multiline message is:</span>
-      <p style="white-space: pre-line;">{{ message }}</p>
-      <textarea v-model="message" placeholder="add multiple lines"></textarea>
-    </div>
-
-    <div>
-      <input type="checkbox" id="checkbox" v-model="checked" />
-      <label for="checkbox">{{ checked }}</label>
-    </div>
-
-    <div>
-      <div>Checked names: {{ checkedNames }}</div>
-
-      <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
-      <label for="jack">Jack</label>
-
-      <input type="checkbox" id="john" value="John" v-model="checkedNames" />
-      <label for="john">John</label>
-
-      <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
-      <label for="mike">Mike</label>
-    </div>
-
-    <div>
-      <div>Picked: {{ picked }}</div>
-
-      <input type="radio" id="one" value="One" v-model="picked" />
-      <label for="one">One</label>
-
-      <input type="radio" id="two" value="Two" v-model="picked" />
-      <label for="two">Two</label>
-    </div>
-
-    <div>
-      <select v-model="selected">
-        <option v-for="option in options" :value="option.value">
-          {{ option.text }}
-        </option>
-      </select>
-
-      <div>Selected: {{ selected }}</div>
-    </div>
-
-    <div>
-      <input
-        type="checkbox"
-        v-model="toggle"
-        :true-value="dynamicTrueValue"
-        :false-value="dynamicFalseValue" />
-      <p>{{toggle}}</p>
+      <button @click="x++">X++</button>
+      <button @click="x++;y++">X and y</button>
     </div>
   </main>
 </template>
