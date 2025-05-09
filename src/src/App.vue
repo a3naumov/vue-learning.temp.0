a94bcb97247d718a0003967d8675659fb040c07d
Artem Naumov <naumovartem272@gmail.com>
2025-05-09 15:32:51 +0400

    Template refs

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,57 +1,17 @@
 <script setup lang="ts">
-import {ref, watch, watchEffect} from "vue";
 
-const question = ref('')
-const answer = ref('Questions usually contain a question mark. ;-)')
-const loading = ref(false)
-watch(question, async (newQuestion, oldQuestion) => {
-  if (newQuestion.includes('?')) {
-    loading.value = true
-    answer.value = 'Thinking...'
-    try {
-      const res = await fetch('https://yesno.wtf/api')
-      answer.value = (await res.json()).answer
-    } catch (error) {
-      answer.value = 'Error! Could not reach the API. ' + error
-    } finally {
-      loading.value = false
-    }
-  }
+import {onMounted, useTemplateRef} from "vue";
+
+const input = useTemplateRef('my-input')
+onMounted(() => {
+  input.value.focus()
 })
-
-
-
-const x = ref(0)
-const y = ref(0)
-watch(x, (newX) => {
-  console.log(`x is ${newX}`)
-}, {once: true})
-watch(
-  () => x.value + y.value,
-  (sum) => {
-    console.log(`sum of x + y is: ${sum}`)
-  }
-)
-watch(
-  [x, () => y.value],
-  ([newX, newY]) => {
-    console.log(`x is ${newX} and y is ${newY}`)
-  }
-)
 </script>
 
 <template>
   <main>
     <div>
-      <p>
-        Ask a yes/no question:
-        <input v-model="question" :disabled="loading">
-      </p>
-      <p>{{answer}}</p>
-    </div>
-    <div>
-      <button @click="x++">X++</button>
-      <button @click="x++;y++">X and y</button>
+      <input ref="my-input">
     </div>
   </main>
 </template>
