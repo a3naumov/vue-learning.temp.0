7149ceb4af367da19bfb12a1a4c445e15aa6c2a7
Artem Naumov <naumovartem272@gmail.com>
2025-05-09 16:30:31 +0400

    Components Basics

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 
-import {onMounted, useTemplateRef} from "vue";
-
-const input = useTemplateRef('my-input')
-onMounted(() => {
-  input.value.focus()
-})
+import ButtonCounter from "./components/ButtonCounter.vue";
 </script>
 
 <template>
   <main>
-    <div>
-      <input ref="my-input">
-    </div>
+      <h1>Here is a child component!</h1>
+      <ButtonCounter title="Button Counter" initial-value="0" @click-event="console.log('Click')">
+        Click
+      </ButtonCounter>
+      <component :is="ButtonCounter">
+        Clickk
+      </component>
   </main>
 </template>
 
---
+++ src/src/components/ButtonCounter.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const props = defineProps(['title', 'initialValue'])
+const emit = defineEmits(['click-event'])
+
+const counter = ref(props.initialValue ?? 5)
+
+const increase = () => {
+  counter.value++
+  emit('click-event')
+}
+</script>
+
+<template>
+  <h2>{{title}}</h2>
+  <p>{{counter}}</p>
+  <button @click="increase"><slot /></button>
+</template>
